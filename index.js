590df619505be91f80867f85be53f4f9130d7963
John Ragbir <johnragbir@gmail.com>
2019-09-03 03:05:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function inc (a) {
+  return ++a
+}
+
+function dec (a) {
+  return --a
+}
+
+add = (a, b) => 
+
+{ return a + b; 
+  
+}
+
+subtract = (a, b) =>
+
+{ return b - a; 
+  
+}
+
+divide = (a, b) =>
+
+{ return a /= b; 
+  
+}
+
+multiply = (a, b) =>
+
+{ return a * b; 
+  
+}
+
+Number.parseInt('100', 10);
